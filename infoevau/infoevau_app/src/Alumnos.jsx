3de340928ae1efa7a6901afc54a91770c7ea0231
Nacho_A_A <nachoalav@uma.es>
2022-11-15 21:28:05 +0100

    añadido navbar y estilos del navbar en css

--- infoevau/infoevau_app/src/Alumnos.jsx
+++ infoevau/infoevau_app/src/Alumnos.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import logo from './assets/images/logoNuevo.png';
 
 function Alumnos() {
 	return (
-		<div>Alumnos</div>
+		<React.StrictMode>
+		<body>
+		  <header id="header">
+		    <nav className='navbar'>
+			  <ul>
+				 <a href="."><img src={logo} className='imgNavbar' alt="" width="50" height="50"/></a>
+              	 <li><a href="/alumnos">Alumnos</a></li>
+              	 <li><a href="/sedes">Sedes</a></li>
+			   </ul>
+			</nav>
+		  </header>
+		</body>
+		</React.StrictMode>
 	)
 }
 
--- infoevau/infoevau_app/src/Home.jsx
+++ infoevau/infoevau_app/src/Home.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import logo from './assets/images/logoNuevo.png';
 
 function Home() {
   return (
     <React.StrictMode>
-      <h1>Bienvenido al main page</h1>
+      <body>
+        <header id='header'>
+         <nav className='navbar'>
+            <ul>
+              <a href="."><img src={logo} className='imgNavbar' alt="" width="50" height="50"/></a>
+              <li><a href="/alumnos">Alumnos</a></li>
+              <li><a href="/sedes">Sedes</a></li>
+            </ul>
+          </nav>
+        </header>
+        <h1>Bienvenido al main page</h1>
+        <h2><a href="./alumnos">Ir a la seccion de alumnos</a></h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+        <h2>Hola</h2>
+      </body>
     </React.StrictMode>
   )
 }
