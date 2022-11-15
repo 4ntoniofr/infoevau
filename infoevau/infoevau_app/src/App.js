23682a19f4d6e478edccb37fe15f601003a8d341
artachojf <pepe.artacho.martin@uma.es>
2022-11-15 14:34:40 +0100

    react router

--- infoevau/infoevau_app/src/App.js
+++ infoevau/infoevau_app/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import Home from './Home';
+import Alumnos from './Alumnos';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+	  path: "/",
+	  element: <Home />,
+	  errorElement: <div>Error Page</div>,
+	},
+	{
+	  path: "/alumnos",
+		element: <Alumnos />,
+		errorElement: <div>Error Page</div>,
+	}
+]);
 
 function App() {
   return (
     <div className="App">
-		InfoEvau
+		<RouterProvider router={router} />
     </div>
   );
 }
