382f36a4417dc015012785ef93787c4bc225e280
4ntoniofr <afr012240@uma.es>
2022-11-16 12:20:27 +0100

    API rechulona

--- infoevau/server/index.js
+++ infoevau/server/index.js
@@ -6,8 +6,8 @@ app.listen(3001, () => {
   console.log("Server running on port 3001");
 });
 
-var mysql = require("mysql");
-var db = mysql.createConnection({
+const mysql = require("mysql");
+const db = mysql.createConnection({
   host: process.env.HOST,
   port: process.env.PORT,
   user: process.env.DB_USER,
@@ -16,12 +16,41 @@ var db = mysql.createConnection({
 });
 db.connect();
 
-/* app.get('/', function (req, res) {
-  res.send('Saludos desde express');
+app.get("/", (req, res) => {
+  dbQuery("SHOW COLUMNS FROM ALUMNO", req, res);
 });
- */
 
-db.query("SHOW TABLES", (err, res, fields) => {
-  if (err) throw err;
-  console.log(res);
+app.get("/alumnos", (req, res) => {
+  dbQuery("SELECT * FROM ALUMNO", req, res);
 });
+
+app.get("/aulas", (req, res) => {
+  dbQuery("SELECT * FROM AULA", req, res);
+});
+
+app.get("/institutos", (req, res) => {
+  dbQuery("SELECT * FROM INSTITUTO", req, res);
+});
+
+app.get("/materias", (req, res) => {
+  dbQuery("SELECT * FROM MATERIA", req, res);
+});
+
+app.get("/matriculas", (req, res) => {
+  dbQuery("SELECT * FROM MATRICULA", req, res);
+});
+
+app.get("/responsables", (req, res) => {
+  dbQuery("SELECT * FROM RESPONSABLE", req, res);
+});
+
+app.get("/sedes", (req, res) => {
+  dbQuery("SELECT * FROM SEDE", req, res);
+});
+
+const dbQuery = (query, req, res) => {
+  db.query(query, (error, results) => {
+    if (error) throw error;
+    res.status(200).json(results);
+  });
+};
