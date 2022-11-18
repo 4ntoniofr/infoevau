badcb1d46e60cc73d863525c4a4de9a12dfc4c7e
4ntoniofr <afr012240@uma.es>
2022-11-18 16:46:03 +0100

    modificando

--- infoevau/infoevau_app/src/services/sedesServices.js
+++ infoevau/infoevau_app/src/services/sedesServices.js
@@ -4,31 +4,37 @@ const insertarSedes = (sedes) => {
   axios.post("http://localhost:3001/nuevasSedes", {
     sedes: sedes.slice(1, sedes.length),
   });
-  return sedes.slice(1, sedes.length).map((sede) => {return {Nombre: sede}});
+  return sedes.slice(1, sedes.length).map((sede) => {
+    return { Nombre: sede };
+  });
 };
 
 const borrarSede = (sede, sedesMemoria) => {
-  if(sede){
-		axios.post("http://localhost:3001/borrarSede", {
-    	sedeBorrar: sede,
-  	});
-  	return sedesMemoria.filter((sedeMemoria) => sedeMemoria.Nombre !== sede);
-	}
-	return sedesMemoria;
+  if (sede) {
+    axios.post("http://localhost:3001/borrarSede", {
+      sedeBorrar: sede,
+    });
+    return sedesMemoria.filter((sedeMemoria) => sedeMemoria.Nombre !== sede);
+  }
+  return sedesMemoria;
 };
 
 const modificarSede = (sede, sedesMemoria) => {
-	if(sede){
-		let sedeModif = prompt('Introduzca un nuevo nombre de sede', sede);
-		axios.post("http://localhost:3001/modificarSede", {
-			prevSede: sede,
-			postSede: sedeModif
-		});
-		sedesMemoria.find(s => s.Nombre === sede).Nombre = sedeModif;
-		console.log(sedesMemoria)
-		return sedesMemoria
-	}
-	return sedesMemoria;
+  if (sede) {
+    let sedeModif = prompt("Introduzca un nuevo nombre de sede", sede);
+    axios.post("http://localhost:3001/modificarSede", {
+      prevSede: sede,
+      postSede: sedeModif,
+    });
+    sedesMemoria.find((s) => s.Nombre === sede).Nombre = sedeModif;
+    return sedesMemoria.map((sedeMemoria) => {
+      if (sedeMemoria.Nombre === sede) {
+        return { ...sedeMemoria, Nombre: sedeModif };
+      }
+      return sedeMemoria;
+    });
+  }
+  return sedesMemoria;
 };
 
 const sedesServices = { insertarSedes, borrarSede, modificarSede };
