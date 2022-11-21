d80e781ade0d03f37ada9faba84149b625af5a2e
Nacho_A_A <nachoalav@uma.es>
2022-11-21 18:37:55 +0100

    avisos alumnos

--- infoevau/infoevau_app/src/components/Alumnos/BotonesAlumnos.jsx
+++ infoevau/infoevau_app/src/components/Alumnos/BotonesAlumnos.jsx
@@ -12,16 +12,25 @@ function BotonesAlumnos({data, setData}) {
          type="file"
          accept=".csv"
          onChange={(e) => {
-            Papa.parse(e.target.files[0], {
-             	complete: (res, file) => {
-             		setData(res);
-            	},
-            });
+			if (e.target.files[0].name.endsWith(".csv")) {
+				Papa.parse(e.target.files[0], {
+					complete: (res, file) => {
+						setData(res);
+				   },
+			   });
+			} else {
+				swal({
+					icon: "error",
+					title: "Formato inválido",
+					text: "El formato del archivo '" + e.target.files[0].name + "' no es válido y no puede importarse."
+				})
+			}
           }}
       />
       <br />
       <button className='buttonAlumnos'
       	onClick={() => {
+			if (data.length > 0) {
 					alumnoServices.insertarAlumnos(data.data.slice(1, data.data.length)).then((r) => {
 						swal({
 							icon: "success",
@@ -29,6 +38,13 @@ function BotonesAlumnos({data, setData}) {
 							text: "Se han insertado correctamente " + r + " de " + (data.data.length-1) + " alumnos posibles"
 						})
 					})
+			} else {
+				swal({
+					icon: "info",
+					title: "No hay datos a importar",
+					text: "Seleccione primero un archivo .csv para cargar los datos"
+				})
+			}
         }}
       >
       	Guardar informacion
