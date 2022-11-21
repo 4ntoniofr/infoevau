51aac0640b84c775366334a7f19104fd30f08898
4ntoniofr <afr012240@uma.es>
2022-11-21 18:56:11 +0100

    home

--- infoevau/infoevau_app/src/components/Home.jsx
+++ infoevau/infoevau_app/src/components/Home.jsx
@@ -1,43 +1,39 @@
 import React from "react";
-import '../assets/css/App.css'
-import logo from '../assets/images/logoNuevo.png'
-import sede from '../assets/images/sedepng.png'
-import estudiante from '../assets/images/estudiantepng.png'
+import "../assets/css/App.css";
+import logo from "../assets/images/logoNuevo.png";
+import sede from "../assets/images/sedepng.png";
+import estudiante from "../assets/images/estudiantepng.png";
 
 export default function Home() {
   return (
     <>
       <div className="divHome">
-
         <h1>Bienvenido a la plataforma infoEVAU</h1>
 
-        <br/>
+        <br />
 
         <img className="imagenLogo" src={logo} alt="logo" />
 
-        <br/>
-        
+        <br />
 
         <div className="twoRows">
+          <a href="./alumnos">
+            <div className="section">
+              <p>Alumnos</p>
+              <img
+                className="imagenSection"
+                alt="alumnosimage"
+                src={estudiante}
+              />
+            </div>
+          </a>
 
-          <div className="section">
-
-            <p>Alumnos</p>
-            <a href="./alumnos">
-              <img className="imagenSection" alt="alumnosimage" src={estudiante} />
-            </a>
-
-          </div>
-
-          <div className="section">
-
-            <p>Sedes</p>
-            <a href="./sedes">
+          <a href="./sedes">
+            <div className="section">
+              <p>Sedes</p>
               <img className="imagenSection" alt="sedesimage" src={sede} />
-            </a>
-
-          </div>
-
+            </div>
+          </a>
         </div>
       </div>
     </>
