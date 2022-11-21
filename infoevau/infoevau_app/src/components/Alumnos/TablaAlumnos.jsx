bff15a78adc69fa444f4f027f45bfd7908835b3d
4ntoniofr <afr012240@uma.es>
2022-11-21 18:20:39 +0100

    cargando

--- infoevau/infoevau_app/src/components/Alumnos/TablaAlumnos.jsx
+++ infoevau/infoevau_app/src/components/Alumnos/TablaAlumnos.jsx
@@ -4,37 +4,45 @@ import axios from "axios";
 
 function TablaAlumnos() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      axios.get("http://localhost:3001/alumnos").then((alumnos) => {
-        setData(alumnos.data);
-      });
+    axios.get("http://localhost:3001/alumnos").then((alumnos) => {
+      setData(alumnos.data);
+      setLoading(false);
+    });
   }, []);
 
   return (
     <div className="containerTabla">
-      <table className="tablaAlumnos">
-        <thead>
-          <tr>
-            <th>Instituto</th>
-            <th>Nombre</th>
-            <th>Apellido1</th>
-            <th>Apellido2</th>
-            <th>DNI/NIF</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((alumno, key) => (
-            <tr key={key}>
-              <td>{alumno.Instituto}</td>
-              <td>{alumno.Nombre}</td>
-              <td>{alumno.Apellido1}</td>
-              <td>{alumno.Apellido2}</td>
-              <td>{alumno.NIF}</td>
+      {loading ? (
+        <p className="loadingText">
+          Cargando alumnos...
+        </p>
+      ) : (
+        <table className="tablaAlumnos">
+          <thead>
+            <tr>
+              <th>Instituto</th>
+              <th>Nombre</th>
+              <th>Apellido1</th>
+              <th>Apellido2</th>
+              <th>DNI/NIF</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data.map((alumno, key) => (
+              <tr key={key}>
+                <td>{alumno.Instituto}</td>
+                <td>{alumno.Nombre}</td>
+                <td>{alumno.Apellido1}</td>
+                <td>{alumno.Apellido2}</td>
+                <td>{alumno.NIF}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
