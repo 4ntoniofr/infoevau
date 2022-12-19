eee661d1cb54021d0c50da16947563d2664ed368
4ntoniofr <afr012240@uma.es>
2022-12-19 17:29:08 +0100

    matriculas

--- infoevau/infoevau_app/src/components/Alumnos/TablaAlumnos.jsx
+++ infoevau/infoevau_app/src/components/Alumnos/TablaAlumnos.jsx
@@ -2,22 +2,34 @@ import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-function TablaAlumnos({data, setData}) {
+function TablaAlumnos({ data, setData }) {
   const [loading, setLoading] = useState(true);
+  const [matriculas, setMatriculas] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:3001/alumnos").then((alumnos) => {
       setData(alumnos.data);
-      setLoading(false);
+      axios.get("http://localhost:3001/matriculas").then((matriculas) => {
+        setMatriculas(matriculas.data);
+        setLoading(false);
+      });
     });
   }, []);
+
+  const getMatriculas = (nif) => {
+    let res = "";
+    matriculas
+      .filter((matricula) => matricula.NIF === nif)
+      .forEach((matricula, i, array) => {
+        res += matricula.Materia + (array.length - 1 !== i ? ", " : "");
+      });
+    return res;
+  };
 
   return (
     <div className="containerTabla">
       {loading ? (
-        <p className="loadingText">
-          Cargando alumnos...
-        </p>
+        <p className="loadingText">Cargando alumnos...</p>
       ) : (
         <table className="tablaAlumnos">
           <thead>
@@ -27,6 +39,7 @@ function TablaAlumnos({data, setData}) {
               <th>Apellido1</th>
               <th>Apellido2</th>
               <th>DNI/NIF</th>
+              <th>Matriculas</th>
             </tr>
           </thead>
           <tbody>
@@ -37,6 +50,7 @@ function TablaAlumnos({data, setData}) {
                 <td>{alumno.Apellido1}</td>
                 <td>{alumno.Apellido2}</td>
                 <td>{alumno.NIF}</td>
+                <td>{getMatriculas(alumno.NIF)}</td>
               </tr>
             ))}
           </tbody>
